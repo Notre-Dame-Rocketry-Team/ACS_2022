6bfc442069d7a852aa1a60004c2240bd00d87672
Daniel Noronha <91227299+noronhadaniel@users.noreply.github.com>
2021-10-24 16:57:59 -0400

    Updated imu.py with an example Python script to be used for data collection after installing Circuit Python and Adafruit_Blinka library.

---
+++ examples/imu.py
@@ -0,0 +1,22 @@
+"""
+This is an example of a data collection Python script for the ICM-20948 IMU.
+Wiring and CircuitPython/Python setup information can be found at:
+https://learn.adafruit.com/adafruit-tdk-invensense-icm-20948-9-dof-imu/python-circuitpython
+"""
+
+# SPDX-FileCopyrightText: 2021 ladyada for Adafruit Industries
+# SPDX-License-Identifier: MIT
+
+import time
+import board
+import adafruit_icm20x
+
+i2c = board.I2C()  # uses board.SCL and board.SDA
+icm = adafruit_icm20x.ICM20948(i2c)
+
+while True:
+    print("Acceleration: X:%.2f, Y: %.2f, Z: %.2f m/s^2" % (icm.acceleration))
+    print("Gyro X:%.2f, Y: %.2f, Z: %.2f rads/s" % (icm.gyro))
+    print("Magnetometer X:%.2f, Y: %.2f, Z: %.2f uT" % (icm.magnetic))
+    print("")
+    time.sleep(0.5)
