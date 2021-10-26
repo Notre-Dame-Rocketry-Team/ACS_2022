fc5aa190c7470fadd8e67c0edce79709b978bcf0
Annika Herko <annikaherko@dhcp-10-7-251-75.dhcp.nd.edu>
2021-10-26 18:24:40 -0400

    added file name function

--- subscale.py
+++ subscale.py
@@ -1,6 +1,7 @@
 # Import libraries
 import sensors
 import logging
+import glob
 
 # Constants
 SAVE_PATH = './data/'
@@ -11,13 +12,34 @@ def find_new_name():
     './data/data_0.csv'
     './data/data_1.csv'
     './data/data_2.csv'
-
+    './data/data_3.csv'
+    './data/data_4.csv'
+    './data/data_5.csv'
+    './data/data_6.csv'
+    './data/data_7.csv'
+    './data/data_8.csv'
+    './data/data_9.csv'
+    './data/data_10.csv'
     # Step 1: search for file names using glob
+    files = glob.glob(SAVE_PATH + SAVE_NAME + "_*" + SAVE_SUFFIX)
 
     # Step 2: if there are any file names, find the biggest number
+    numbers= []
 
+    for x in files: 
+        y=x.replace(SAVE_PATH + SAVE_NAME + "_", "").replace(SAVE_SUFFIX, "")
+        int(y)
+        y=int(y)
+        numbers.append(y)
+    z=max(numbers)
+    a=(SAVE_PATH + SAVE_NAME + "_"+z + SAVE_SUFFIX)
+    return a
+        
+    
     # Step 3: find new file name
-    pass
+    import glob
+        glob.glob('./data/data_(*-1).csv')
+        return: max
 
 def initialize_sensors():
     pass
