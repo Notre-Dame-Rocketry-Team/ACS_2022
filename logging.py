933b9f1d3f2fe2aea028bda026d8539a6f7397de
Hector Juarez <hjuarez2@nd.edu>
2021-10-26 17:54:30 -0400

    first commit

---
+++ logging.py
@@ -0,0 +1,14 @@
+import csv
+
+
+#creates a new CSV file
+#input: name of CSV file, list of headers
+#output: none
+def newCSV():
+    f = open('path/to/csv_file', 'w')
+
+#adds a row of data to a given CSV file
+#input: row of data as a list
+#output: none
+def addRow():
+    xx
