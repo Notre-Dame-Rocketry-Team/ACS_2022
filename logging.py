4c0f4636613eb22d239ed584b2c9c617f27c75e2
Hector Juarez <hjuarez2@nd.edu>
2021-10-26 18:06:33 -0400

    draft 1

--- logging.py
+++ logging.py
@@ -1,14 +1,33 @@
+# NDRT 2021-2022
+# ACS subteam
+# This code contains functions to create CSV files and add data to them 
+
 import csv
 
+# creates a new CSV file
+# input: filepath to new CSV file, list of headers as a python list
+# returns: n/a
+def newCSV(filepath,headers):
+    
+    # open file
+    with open(filepath, 'w') as f:
+  
+        # create the csv writer
+        writer = csv.writer(f)
 
-#creates a new CSV file
-#input: name of CSV file, list of headers
-#output: none
-def newCSV():
-    f = open('path/to/csv_file', 'w')
+        # write a row to the csv file
+        writer.writerow(headers)
 
 #adds a row of data to a given CSV file
-#input: row of data as a list
-#output: none
-def addRow():
-    xx
+#input: filepath to new CSV file, row of data as a python list
+#output: n/a
+def addRow(filepath,row):
+    
+    # open file
+    with open(filepath, 'w') as f:
+    
+        # create the csv writer
+        writer = csv.writer(f)
+
+        # write a row to the csv file
+        writer.writerow(headers)
