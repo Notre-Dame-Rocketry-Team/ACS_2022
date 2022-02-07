fb15b89918410ea2bdf4f134e66dc5f0bacb152a
Daniel Noronha <91227299+noronhadaniel@users.noreply.github.com>
2022-02-07 18:00:49 -0500

    Testing

--- examples/imu.py
+++ examples/imu.py
@@ -16,6 +16,7 @@ icm = adafruit_icm20x.ICM20948(i2c)
 
 while True:
     print("Acceleration: X:%.2f, Y: %.2f, Z: %.2f m/s^2" % (icm.acceleration))
+    print(type(icm.acceleration))
     print("Gyro X:%.2f, Y: %.2f, Z: %.2f rads/s" % (icm.gyro))
     print("Magnetometer X:%.2f, Y: %.2f, Z: %.2f uT" % (icm.magnetic))
     print("")
