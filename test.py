c6f9127bf75c33b1c201cbb272cdc90a0e6a13b7
Patrick Faley <pfaley@nd.edu>
2022-02-10 17:31:19 -0600

    testing

--- test.py
+++ test.py
@@ -2,15 +2,16 @@ import time
 from adafruit_servokit import ServoKit
 CONTROLLER_PIN = 5
 kit = ServoKit(channels=16)
-servo = kit.continuous_servo[CONTROLLER_PIN]
 try:
     while True:
-        servo.throttle = -1
-        print(f"Servo Throttle: {servo.throttle} = {servo.throttle * 100}%")
+        kit.continuous_servo[5].throttle = -0.8
+        #print(f"Servo Throttle: {servo.throttle} = {servo.throttle * 100}%")
+        time.sleep(1)
 except KeyboardInterrupt:
-    servo.throttle = 0
-    servo.throttle = 0
-    servo.throttle = 0
-    print(f"Servo Throttle: {servo.throttle} = {servo.throttle * 100}%")
+    #servo.throttle = 0
+    #servo.throttle = 0
+    #servo.throttle = 0
+    #print(f"Servo Throttle: {servo.throttle} = {servo.throttle * 100}%")
+    pass
 
 
