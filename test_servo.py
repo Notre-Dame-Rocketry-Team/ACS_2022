a9edd545ebe1bb24ac196f7c9e4ee0d3fe9c35bc
Daniel Noronha <91227299+noronhadaniel@users.noreply.github.com>
2022-02-10 20:33:49 -0500

    testing servo

---
+++ test_servo.py
@@ -0,0 +1,19 @@
+import time
+import sys
+import board
+import busio
+import adafruit_pca9685
+from adafruit_servokit import ServoKit
+
+i2c = busio.I2C(board.SCL, board.SDA)
+pca = adafruit_pca9685.PCA9685(i2c)
+#pca.frequency = sys.argv[2]
+CONTROLLER_PIN = 5
+STOP = 0.15
+kit = ServoKit(channels=16)
+servo = kit.continuous_servo[CONTROLLER_PIN]
+
+
+while True:
+    servo.throttle = sys.argv[1]
+
