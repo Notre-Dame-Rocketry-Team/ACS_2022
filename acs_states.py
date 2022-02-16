b03f266a81577078f2add288a6ace25c52d0d0b0
Daniel Noronha <91227299+noronhadaniel@users.noreply.github.com>
2022-02-16 18:50:37 -0500

    pushing

--- acs_states.py
+++ acs_states.py
@@ -66,10 +66,16 @@ def acs_active(manager: Data_Manager):
     if acs_timer_start == None:
         acs_timer_start = time.time()
         controller_servo.servo_up(manager)
+        print("SERVO TIMER START/SERVO UP")
+        print(time.time()-acs_timer_start)
     elif time.time()-acs_timer_start >= 5:
+        print("SERVO DOWN")
         controller_servo.servo_down(manager)
-    elif time.time()-acs_timer_start >= 10:
+        print(time.time()-acs_timer_start)
+    elif time.time()-acs_timer_start >= 7:
+        print("SERVO STOP")
         controller_servo.servo_stop(manager)
+        print(time.time()-acs_timer_start)
 
     acs_state = acs_states[2] # ACS_Active
     manager.update_field('ACS_state',acs_state)
