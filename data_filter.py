4ff5b6b6aaec6388b83d98b9707b6b0d66746f85
Daniel Noronha <91227299+noronhadaniel@users.noreply.github.com>
2022-02-18 20:12:41 -0500

    Update data_filter.py

--- data_filter.py
+++ data_filter.py
@@ -95,14 +95,14 @@ def get_dt(in_time):
     return dt
 
 def transform_LIS(in_accel):
-    out_accel = float(in_accel[3])#-9.5244
+    out_accel = float(in_accel[2])#-9.5244
     #print(f'ADXL: {in_accel[2]}, {out_accel}')
     return out_accel
 
 def transform_ICM(in_accel):
     #out_accel = g*(float(in_accel[2]))-0.53
     if FAKE_DATA:
-        out_accel = (float(in_accel[3]))# + 9.80665?
+        out_accel = (float(in_accel[2])) + g
     else:
         out_accel = (float(in_accel[0]))-9.795813967536112
     #print(f'MPU: {in_accel[2]}, {out_accel}')
