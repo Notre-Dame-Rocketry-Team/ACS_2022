ef8b48752327d7d33f8fa932ce062f0f2f5ca619
Daniel Noronha <91227299+noronhadaniel@users.noreply.github.com>
2022-02-27 18:09:35 -0500

    Update init.py

--- init.py
+++ init.py
@@ -1,10 +1,12 @@
 import beep
-import time
+# import time
 
 beep.init()
-beep.beep(1000, 1)
-beep.beep(1200, 1)
-beep.beep(1400, 1)
-beep.beep(1600, 1)
-beep.beep(1800, 1)
+beep.beep(1046.5, 1)#C
+beep.beep(1174.7, 1)#D
+beep.beep(1318.5, 1)#E
+beep.beep(1396.9, 1)#F
+beep.beep(1568, 1)#G
+beep.beep(1760, 1)#A
+beep.beep(1975.5, 1)#B
 # time.sleep(5)
