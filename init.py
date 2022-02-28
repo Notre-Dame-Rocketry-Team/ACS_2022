904cb9c19968bf7a0924656e284aab8ae111cca1
Patrick Faley <pfaley@nd.edu>
2022-02-28 15:36:21 -0600

    arpeggio

--- init.py
+++ init.py
@@ -3,10 +3,10 @@ import beep
 
 beep.init()
 beep.beep(1046.5, 1)#C
-beep.beep(1174.7, 1)#D
+#beep.beep(1174.7, 1)#D
 beep.beep(1318.5, 1)#E
-beep.beep(1396.9, 1)#F
+#beep.beep(1396.9, 1)#F
 beep.beep(1568, 1)#G
-beep.beep(1760, 1)#A
-beep.beep(1975.5, 1)#B
+#beep.beep(1760, 1)#A
+#beep.beep(1975.5, 1)#B
 # time.sleep(5)
