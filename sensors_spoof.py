ab2b725af0c16e2d5135166ac3ab9b2280a00fe8
Daniel Noronha <91227299+noronhadaniel@users.noreply.github.com>
2022-03-31 15:56:48 -0400

    Added Kalman velocity as a fake sensor

--- sensors_spoof.py
+++ sensors_spoof.py
@@ -10,6 +10,7 @@ acce = None
 altimeter = None
 icm = None
 times = None
+kalman_velocity = None
 
 read_row = lambda data, key: [row[key] for row in data]
 
@@ -27,6 +28,7 @@ ICM_GYRO_Z = 'ICM_gyroscope_z'
 ICM_MAGN_X = 'ICM_magnetometer_x'
 ICM_MAGN_Y = 'ICM_magnetometer_y'
 ICM_MAGN_Z = 'ICM_magnetometer_z'
+KALMAN_VELOCITY = 'Kalman_velocity'
 
 def init_time(manager: Data_Manager, rows) -> bool:
     manager.add_data(data_manager.Scalar_Data('Time'))
@@ -104,6 +106,18 @@ def read_altimeter(manager: Data_Manager):
     manager.update_field('BMP_altitude', altitude)
     # print(f"Altitude: {altitude}")
 
+def init_kalman_velocity(manager: Data_Manager, rows) -> bool:
+    global kalman_velocity
+
+    manager.add_data(data_manager.Scalar_Data('Kalman_velocity'))
+    kalman_velocity = iter(read_row(rows, KALMAN_VELOCITY))
+    return True
+
+def read_kalman_velocity(manager: Data_Manager):
+    global kalman_velocity
+    vel = next(kalman_velocity)
+    manager.update_field('Kalman_velocity', vel)
+
 def initialize_sensors(path: str, manager: Data_Manager) -> bool:
     with open(path, newline='') as f:
         reader = csv.DictReader(f)
@@ -116,6 +130,7 @@ def initialize_sensors(path: str, manager: Data_Manager) -> bool:
             result = result and init_accelerometer(manager, rows)
         elif sensor == 'Altimeter':
             result = result and init_altimeter(manager, rows)
+    result = result and init_kalman_velocity(manager, rows)
     
     return result
 
@@ -134,4 +149,5 @@ def read_sensors(manager: Data_Manager):
             read_accelerometer(manager)
         elif sensor == 'Altimeter':
             read_altimeter(manager)
+    read_kalman_velocity(manager)
 
