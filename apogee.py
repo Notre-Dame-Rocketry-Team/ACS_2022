577d72cb80332ff10b8adf9256d61aa01ef5cd7d
Daniel Noronha <91227299+noronhadaniel@users.noreply.github.com>
2022-04-04 02:25:19 -0400

    Update apogee.py

--- apogee.py
+++ apogee.py
@@ -7,7 +7,7 @@ with open("data_fullscale_20220301_01.csv",'r') as f:
     for line in data:
             split_data = str(line).split(',')
             if len(split_data) >= 7:
-                altdata.append(float(split_data[-6]))
+                altdata.append(float(split_data[13]))
             #print(line)
             #altdata.append(str(line).split(',')[-2])
     print(f"Maximum Kalman Altitude (Apogee) = {max(altdata)}m = {max(altdata) * 3.28084}ft.")
