c170eca3f8b712ba59499f98526a881867914c6c
Patrick Faley <pfaley@nd.edu>
2022-04-17 16:09:13 -0500

    changed servo motor pin

--- test_servo.py
+++ test_servo.py
@@ -5,7 +5,7 @@ from adafruit_servokit import ServoKit
 #i2c = busio.I2C(board.SCL, board.SDA)
 #pca = adafruit_pca9685.PCA9685(i2c)
 #pca.frequency = float(sys.argv[2]
-CONTROLLER_PIN = 5
+CONTROLLER_PIN = 7
 STOP = 0.15
 kit = ServoKit(channels=16)
 servo = kit.continuous_servo[CONTROLLER_PIN]
